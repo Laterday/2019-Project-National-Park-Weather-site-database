3e45b5f5ebdadca0ad25d6c01a45ff2e2cb24ca8
Kyle Thomas <k-thomas.5@onu.edu>
2019-03-13 13:57:51 -0400

    ParkSQLDAL created, IParkSQLDAL created, GetParks method created

---
+++ 13-Capstone/Capstone.Web/DAL/ParkSQLDAL.cs
@@ -0,0 +1,66 @@
+﻿using Capstone.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.DAL
+{
+    public class ParkSQLDAL
+    {
+        private string connectionString;
+
+        private const string SQL_GetParks = "SELECT * FROM park;";
+
+        public ParkSQLDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<Park> GetParks()
+        {
+            List<Park> parks = new List<Park>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand(SQL_GetParks, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Park park = new Park();
+
+                        park.ParkCode = Convert.ToString(reader["parkCode"]);
+                        park.ParkName = Convert.ToString(reader["parkName"]);
+                        park.State = Convert.ToString(reader["state"]);
+                        park.Acreage = Convert.ToInt32(reader["acreage"]);
+                        park.ElevationInFeet = Convert.ToInt32(reader["elevationInFeet"]);
+                        park.MilesOfTrail = Convert.ToInt32(reader["milesOfTrail"]);
+                        park.NumberOfCampsites = Convert.ToInt32(reader["numberOfCampsites"]);
+                        park.Climate = Convert.ToString(reader["climate"]);
+                        park.YearFounded = Convert.ToInt32(reader["yearFounded"]);
+                        park.AnnualVisitorCount = Convert.ToInt32(reader["annualVisitorCount"]);
+                        park.InspirationalQuote = Convert.ToString(reader["inspirationalQuote"]);
+                        park.InspirationalQuoteSource = Convert.ToString(reader["inspirationalQuoteSource"]);
+                        park.ParkDescription = Convert.ToString(reader["parkDescription"]);
+                        park.EntryFee = Convert.ToInt32(reader["entryFee"]);
+                        park.NumberOfAnimalSpecies = Convert.ToInt32(reader["numberOfAnimalSpecies"]);
+
+                        parks.Add(park);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                parks = new List<Park>();
+            }
+
+            return parks;
+        }
+    }
+}
