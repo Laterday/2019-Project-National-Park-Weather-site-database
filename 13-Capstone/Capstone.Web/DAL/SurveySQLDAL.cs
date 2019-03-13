6d0243667ce0abef992dbf202b841869c3838d2f
Shep Haines <shephaines22@gmail.com>
2019-03-13 17:56:42 -0400

    layout and surveysql setup

---
+++ 13-Capstone/Capstone.Web/DAL/SurveySQLDAL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.DAL
+{
+    public class SurveySQLDAL
+    {
+        private string connectionString;
+
+        private const string SQL_GetSurveyCount = ""
+    }
+}
