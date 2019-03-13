820a6cc0549d5913ea9b8c7e881476f828c823ed
Kyle Thomas <k-thomas.5@onu.edu>
2019-03-13 18:52:02 -0400

    made changes to weather model and home/detail view

--- 13-Capstone/Capstone.Web/Models/Weather.cs
+++ 13-Capstone/Capstone.Web/Models/Weather.cs
@@ -12,52 +12,109 @@ namespace Capstone.Web.Models
         public int Low { get; set; }
         public int High { get; set; }
         public string Forecast { get; set; }
-        public string Recommendation { get; set; }
+        public string PictureName
+        {
+            get
+            {
+                if (Forecast == "partly cloudy")
+                {
+                    return "partlyCloudy";
+                }
+                else
+                {
+                    return Forecast;
+                }
+            }
+            set { }
+        }
+        public string Recommendation
+        {
+            get
+            {
+                string result = "Be sure to ";
+
+                switch (Forecast)
+                {
+                    case "snow":
+                        result += "pack snowshoes.";
+                        break;
+                    case "rain":
+                        result += "pack rain gear and wear waterproof shoes.";
+                        break;
+                    case "thunderstorms":
+                        result += "seek shelter and avoid hiking on exposed ridges.";
+                        break;
+                    case "sunny":
+                        result += "pack sunblock.";
+                        break;
+                    default:
+                        result += "enjoy the cloudy day.";
+                        break;
+                }
+
+                if (High > 75)
+                {
+                    result += " Since it will be so warm out, bringing an extra gallon of water is recommended.";
+                }
+
+                if (Low < 20)
+                {
+                    result += " Since it will be so cold out, be aware that sustained exposure to frigid temperatures is dangerous.";
+                }
+
+                if (Math.Abs(High - Low) > 20)
+                {
+                    result += " It may be worthwhile to dress in breathable layers due to the variation in temperature throughout the day.";
+                }
+
+                return result;
+            }
+        }
 
         public static double FarenheitToCelsius(int farenheitValue)
         {
-            return ((farenheitValue - 32) * 5/9);
+            return (double)((farenheitValue - 32) * 5/9);
         }
 
-        public static string GetRecommendation(string forecast, int high, int low)
-        {
-            string result = "Be sure to ";
+        //public static string GetRecommendation(string forecast, int high, int low)
+        //{
+        //    string result = "Be sure to ";
 
-            switch (forecast)
-            {
-                case "snow":
-                    result += "pack snowshoes.";
-                    break;
-                case "rain":
-                    result += "pack rain gear and wear waterproof shoes.";
-                    break;
-                case "thunderstorms":
-                    result += "seek shelter and avoid hiking on exposed ridges.";
-                    break;
-                case "sunny":
-                    result += "pack sunblock.";
-                    break;
-                default:
-                    result += "enjoy the cloudy day.";
-                    break;
-            }
+        //    switch (forecast)
+        //    {
+        //        case "snow":
+        //            result += "pack snowshoes.";
+        //            break;
+        //        case "rain":
+        //            result += "pack rain gear and wear waterproof shoes.";
+        //            break;
+        //        case "thunderstorms":
+        //            result += "seek shelter and avoid hiking on exposed ridges.";
+        //            break;
+        //        case "sunny":
+        //            result += "pack sunblock.";
+        //            break;
+        //        default:
+        //            result += "enjoy the cloudy day.";
+        //            break;
+        //    }
 
-            if (high > 75)
-            {
-                result += " Since it will be so warm out, bringing an extra gallon of water is recommended.";
-            }
+        //    if (high > 75)
+        //    {
+        //        result += " Since it will be so warm out, bringing an extra gallon of water is recommended.";
+        //    }
 
-            if (low < 20)
-            {
-                result += " Since it will be so cold out, be aware that sustained exposure to frigid temperatures is dangerous.";
-            }
+        //    if (low < 20)
+        //    {
+        //        result += " Since it will be so cold out, be aware that sustained exposure to frigid temperatures is dangerous.";
+        //    }
 
-            if (Math.Abs(high - low) > 20)
-            {
-                result += " It may be worthwhile to dress in breathable layers due to the variation in temperature throughout the day.";
-            }
+        //    if (Math.Abs(high - low) > 20)
+        //    {
+        //        result += " It may be worthwhile to dress in breathable layers due to the variation in temperature throughout the day.";
+        //    }
 
-            return result;
-        }
+        //    return result;
+        //}
     }
 }
