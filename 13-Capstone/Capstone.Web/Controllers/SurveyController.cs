8f1e7b539755d778da70ab0a224db48606075082
Kyle Thomas <k-thomas.5@onu.edu>
2019-03-13 22:25:16 -0400

    Additions to SurveyController

--- 13-Capstone/Capstone.Web/Controllers/SurveyController.cs
+++ 13-Capstone/Capstone.Web/Controllers/SurveyController.cs
@@ -2,16 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Capstone.Web.DAL.Interfaces;
+using Capstone.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Capstone.Web.Controllers
 {
     public class SurveyController : Controller
     {
+        private ISurveySQLDAL surveySQLDAL;
+
+        public SurveyController(ISurveySQLDAL surveySQLDAL)
+        {
+            this.surveySQLDAL = surveySQLDAL;
+        }
+
         [HttpGet]
         public IActionResult Submit()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Submit(Survey survey)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(survey);
+            }
+            else
+            {
+                bool result = surveySQLDAL.AddSurvey(survey);
+                return RedirectToAction(nameof(ViewSurveyResults));
+            }
+        }
+
+        [HttpGet]
+        public IActionResult ViewSurveyResults()
+        {
+            List<SurveyResult> results = surveySQLDAL.GetSurveyCount();
+            return View(results);
         }
     }
 }
