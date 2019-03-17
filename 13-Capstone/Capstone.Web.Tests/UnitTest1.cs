9b8edd622e9340654db604851e2b1f9e8f4e8eb3
Kyle Thomas <k-thomas.5@onu.edu>
2019-03-17 15:40:50 -0400

    started tests

---
+++ 13-Capstone/Capstone.Web.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//namespace Capstone.Web.Tests
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        private TransactionScope tran;
+//        private string connectionString;
+
+//        [TestInitialize]
+//        public void Initialize()
+//        {
+//            tran = new TransactionScope();
+
+//            using (SqlConnection connection = new SqlConnection(connectionString))
+//            {
+//                SqlCommand command;
+//                connection.Open();
+//            }
+//        }
+
+//        [TestMethod]
+//        public void TestMethod1()
+//        {
+//        }
+//    }
+//}
