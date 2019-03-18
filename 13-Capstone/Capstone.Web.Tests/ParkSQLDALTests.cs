d994589430ace3d73afc624b84de87e96aee0da6
Shep Haines <shephaines22@gmail.com>
2019-03-17 22:33:39 -0400

    submit results view

---
+++ 13-Capstone/Capstone.Web.Tests/ParkSQLDALTests.cs
@@ -0,0 +1,70 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Transactions;
+using Capstone.Web.Models;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using Capstone.Web.DAL;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Capstone.Web
+{
+    [TestClass]
+    public class ParkSQLDALTests
+    {
+        private TransactionScope transaction;
+        private string connectionString = @"Data Source=.\sqlexpress;Initial Catalog=NPGeek;Integrated Security=True";
+        private int affectedRows;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            transaction = new TransactionScope();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("INSERT INTO park VALUES ('FP', 'Fake Park', 'Ohio', 100, 100, 100, 100, 'Warm', 2000, 100, 'Visit soon!', 'Bono', 'A really cool park.', 1, 100)", connection);
+
+                affectedRows = (int)command.ExecuteNonQuery();
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            transaction.Dispose();
+        }
+
+        [TestMethod]
+        public void GetParksTest()
+        {
+            ParkSQLDAL park = new ParkSQLDAL(connectionString);
+            List<Park> parks = park.GetParks();
+
+            Assert.AreEqual(1, affectedRows);
+            Assert.IsTrue(parks.Count >= 1);
+        }
+
+        [TestMethod]
+        public void GetUniqueParkNamesTest()
+        {
+            ParkSQLDAL park = new ParkSQLDAL(connectionString);
+            List<SelectListItem> parks = park.GetUniqueParkNames();
+
+            Assert.AreEqual(1, affectedRows);
+            Assert.IsTrue(parks.Count >= 1);
+        }
+
+        [TestMethod]
+        public void GetParkDetailsTest()
+        {
+            ParkSQLDAL park = new ParkSQLDAL(connectionString);
+            Park newPark = park.GetParkDetails("FP");
+
+            Assert.AreEqual(1, affectedRows);
+            Assert.AreEqual("Fake Park", newPark.ParkName);
+        }
+    }
+}
---
+++ 13-Capstone/Capstone.Web.Tests/SurveySQLDALTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Transactions;
+using Capstone.Web.Models;
+using System.Data.SqlClient;
+using Capstone.Web.DAL;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Capstone.Web.Tests
+{
+    [TestClass]
+    public class SurveySQLDALTests
+    {
+        private TransactionScope transaction;
+        private string connectionString = @"Data Source=.\sqlexpress;Initial Catalog=NPGeek;Integrated Security=True";
+        private int affectedRows;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            transaction = new TransactionScope();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("INSERT INTO survey_result (parkCode, emailAddress, state, activityLevel) VALUES ('YNP', 'g@gmail.com', 'OH', 'Active')", connection);
+
+                affectedRows = (int)command.ExecuteNonQuery();
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            transaction.Dispose();
+        }
+
+        [TestMethod]
+        public void GetSurveyCountTest()
+        {
+            SurveySQLDAL survey = new SurveySQLDAL(connectionString);
+            List<SurveyResult> result = survey.GetSurveyCount();
+
+            Assert.AreEqual(1, affectedRows);
+            Assert.IsTrue(result.Count >= 1);
+        }
+
+        [TestMethod]
+        public void AddSurveyTest()
+        {
+            SurveySQLDAL surveyDAL = new SurveySQLDAL(connectionString);
+
+            Survey survey = new Survey
+            {
+                ParkCode = "YNP",
+                EmailAddress = "k@gmail.com",
+                State = "OH",
+                ActivityLevel = "Active"
+            };
+
+            bool result = surveyDAL.AddSurvey(survey);
+            
+            Assert.IsTrue(result);
+        }
+    }
+}
